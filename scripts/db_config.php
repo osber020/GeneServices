2c9c295c1adb1341b5fe09d646a65877c6d366e5
John.Osberg <J_Osberg@outlook.com>
2015-09-22 20:37:01 -0500

    added php scripts.. again..

--- scripts/db_config.php
+++ scripts/db_config.php
@@ -1,13 +1,13 @@
 db_config.php
 <?php
- 
+
 /*
- * All database connection variables
- */
- 
-define('DB_USER', "root"); // db user
-define('DB_PASSWORD', "Pikachu@2"); // db password (mention your db password here)
-define('DB_DATABASE', "geneserver"); // database name
-define('DB_SERVER', "localhost"); // db server
-define('DB_PORT', "9001");
+* All database connection variables
+*/
+
+define('DB_USER', "b5daa5ada4796c"); // db user
+define('DB_PASSWORD', "3d81114b"); // db password (mention your db password here)
+define('DB_DATABASE', "GenesisDB"); // database name
+define('DB_SERVER', "us-cdbr-azure-central-a.cloudapp.net"); // db server
+define('DB_PORT', "3306");
 ?>

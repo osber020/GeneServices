9b12988ed8080a5cfec76e64c50bc9c120ab5ac5
John.Osberg <J_Osberg@outlook.com>
2015-09-22 21:28:54 -0500

    Trying to figure out the cloud

--- scripts/get_song.php
+++ scripts/get_song.php
@@ -4,23 +4,23 @@ get_song.php
   * This function will retrieve the next song that the DJ is currently listening to and the
   * listener will hear next.
   */
-  
+
   $response = array();
 
  if(isset($_POST['sessionID']))
  {
 	$sessionID = $_POST['sessionID'];
-	
+
 	//Connect to db, because abstraction is for pussies.
 	require_once __DIR__ . '/db_config.php';
 	$db = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT) or die(mysqli_error());
-	
+
 	$result = mysqli_query($db, "SELECT songHash FROM dj WHERE sessionID = " + $sessionID + ";");
-	
-	if (!empty($result)) 
+
+	if (!empty($result))
 	{
         // check for empty result
-        if (mysqli_num_rows($result) > 0) 
+        if (mysqli_num_rows($result) > 0)
 		{
             $result = mysqli_fetch_array($result);
 			$profile = array();
@@ -30,6 +30,14 @@ get_song.php
 			echo json_encode($response);
 		}
 	}
+    else {
+        $result = "Words Go here";
+        $profile = array();
+        $profile["songHash"] = "This is where a response should go";
+        $response["next"] = array();
+        array_push($response["next"], $profile);
+        echo json_encode($response);
+    }
 	mysqli_close($db);
  }
 ?>
